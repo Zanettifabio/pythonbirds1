b3e72a5efeace80f64fae89ae02b091192483357
Zanettifabio <fczanetti@hotmail.com>
2023-10-05 12:46:24 -0300

    teste_status de obstáculo e porco validado

--- atores.py
+++ atores.py
@@ -60,11 +60,12 @@ class Ator():
 
 
 class Obstaculo(Ator):
-    pass
+    _caracter_ativo = 'O'
 
 
 class Porco(Ator):
-    pass
+    _caracter_ativo = '@'
+    _caracter_destruido = '+'
 
 
 class DuploLancamentoExcecao(Exception):
